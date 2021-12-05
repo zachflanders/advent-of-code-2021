91ce80f335793db8a74ff9e1a3e6cf50d0589dd1
Zach Flanders <zflanders@mysidewalk.com>
2021-12-04 21:44:38 -0600

    refactoring 4

--- solutions/day_4.py
+++ solutions/day_4.py
@@ -1,5 +1,6 @@
 import pandas
 import requests
+from collections import OrderedDict
 
 from settings import cookie
 
@@ -8,33 +9,30 @@ class BingoSubsystem:
 
     def __init__(self):
         raw_input = requests.get(
-                'https://adventofcode.com/2021/day/4/input',
-                headers={'cookie': cookie}
-            ).text.split('\n')[:-1]
+            'https://adventofcode.com/2021/day/4/input',
+            headers={'cookie': cookie}
+        ).text.split('\n')[:-1]
         self.numbers = [int(num) for num in raw_input[0].split(',')]
-        self.boards = []
-        for i, row in enumerate(raw_input):
-            if row == '':
-                board = pandas.concat([
-                    pandas.DataFrame(data=[int(num) for num in row.split(' ') if num != '']).T
-                    for row in raw_input[i+1:i+6]
-                ]).reset_index(drop=True)
-                self.boards.append(board)
+        self.boards = [
+            pandas.concat([
+                pandas.DataFrame(data=[int(num) for num in row.split(' ') if num != '']).T
+                for row in raw_input[i + 1:i + 6]
+            ]).reset_index(drop=True)
+            for i, row in enumerate(raw_input) if row == ''
+        ]
         self.marks = [
             pandas.DataFrame({i: [False, False, False, False, False] for i in range(0, 5)})
             for _ in range(0, len(self.boards))
         ]
 
     def play(self):
-        winning_boards = []
-        winning_scores = []
+        wins = OrderedDict()
         for num in self.numbers:
             for i, board in enumerate(self.boards):
                 self.marks[i] = self.marks[i] + board.isin([num])
-                if i not in winning_boards and (self.marks[i].all().any() or self.marks[i].all(axis='columns').any()):
-                    winning_boards.append(i)
-                    winning_scores.append(self.calc_score(num, board, self.marks[i]))
-        return winning_scores[0], winning_scores[-1]
+                if i not in wins and (self.marks[i].all().any() or self.marks[i].all(axis='columns').any()):
+                    wins[i] = self.calc_score(num, board, self.marks[i])
+        return wins.popitem(last=False)[1], wins.popitem()[1]
 
     @staticmethod
     def calc_score(num, board, marks):
